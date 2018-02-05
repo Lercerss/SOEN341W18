a5dac3f227146dc3ece15a048cd9e661416c3ce2
jetcanfly <play_workhard@hotmail.com>
2018-02-05 02:04:26 -0500

    #21 use a customized form

---
+++ SA3/qa_web/forms.py
@@ -0,0 +1,23 @@
+__author__ = 'jet'
+
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        label='username',
+        error_messages={'required': 'please input username'},
+
+    )
+    password = forms.CharField(
+        required=True,
+        label='password',
+        error_messages={'required': u'please input password'},
+    )
+
+    def clean(self):
+        if not self.is_valid():
+            raise forms.ValidationError("username and password are required")
+        else:
+            cleaned_data = super(LoginForm, self).clean()
