d8a113348b78c856e542a2540aad6df03db3d87a
Alvyn <alvyn279@gmail.com>
2018-02-10 14:49:29 -0500

    Implemented core database tables
    - Added necessary classes and relational structure to models.py
    - Satisfy issue #35

--- SA3/qa_web/admin.py
+++ SA3/qa_web/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
-from .models import Questions
+from .models import Post, Questions, Answers, Comments, Tags
 
 admin.site.register(Post)
 admin.site.register(Questions)
+admin.site.register(Answers)
+admin.site.register(Comments)
+admin.site.register(Tags)
--- SA3/qa_web/models.py
+++ SA3/qa_web/models.py
@@ -3,12 +3,48 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    content = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    """
+    Parent meta class that describes a post as a publication on the website with certain content
+    created by a certain owner
+    This will be used for multi-table inheritance
+    """
+    content = models.TextField(null=True)
+    owner = models.ForeignKey(User, null= True, on_delete=models.SET_NULL)
     creation_date = models.DateTimeField(auto_now_add=True)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
 
 class Questions(Post):
-    title = models.CharField(max_length=300)
+    """
+    The question (or thread) initiates the interaction between users' answers,
+    comments, and voting
+    """
+    title = models.CharField(max_length=300, null=True)
+    visits = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.title
 
+class Answers(Post):
+    """
+    The answer is a proposed solution to a certain question
+    There can be many answers to a question and there is only one correct answer per question
+    """
+    question = models.ForeignKey(Questions, null=True, on_delete=models.CASCADE)
+    correct_answer = models.BooleanField(default = False)
+
+class Comments(Post):
+    """
+    A comment can be posted for an entire question or an answer
+    There can be many comments to questions and answers
+    """
+    question = models.ForeignKey(Questions, null=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answers, null=True, on_delete=models.CASCADE)
+    
+class Tags(models.Model):
+    """
+    One or multiple tags can be assigned to a question, in order to classify the question
+    There can be many tags to a question
+    """
+    question = models.ForeignKey(Questions, null= True, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, null=True)
