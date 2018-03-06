f98a481d2588fef5f105ff4b99c44698b0d818d4
Lucas Turpin <lucas_turpin@hotmail.com>
2018-03-06 18:37:36 -0500

    Modularized tests and separation by component #75
    	- Moved tests.py to new qa_web/tests folder
    	- Separated test cases by model, view and form components
    	- Added stubs for views test cases

---
+++ SA3/qa_web/tests/test_forms.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from qa_web.forms import UserProfile
+
+
+# Create your tests here.
+class FormTest(TestCase):
+
+    def test_user_profile(self):
+        form_data = {'prename': 'John', 'surname': 'Smith',
+                     'email': 'test@example.com', 'age': 40, 'birthday': '1960-08-24',
+                     'motherland': 'le quebec', 'school': 'McGill University',
+                     'major': 'COMP SCI', 'city': 'Montreal'}
+        form = UserProfile(data=form_data)
+        self.assertTrue(form.is_valid())
---
+++ SA3/qa_web/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from qa_web.views import index, QuestionDisplayView
+
+
+class ViewTest(TestCase):
+    """This class contains test methods for view functions"""
+
+    def test_index(self):
+        pass
+
+
+class QuestionDisplayViewTest(TestCase):
+    """This class contains test cases for the QuestionDisplayView"""
+
+    def test_get_context(self):
+        pass
