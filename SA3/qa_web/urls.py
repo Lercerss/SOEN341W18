1d131f7bfcbf0b73165b719b9cf19cf6e55615fa
niiita <nitachaska@gmail.com>
2018-03-07 16:16:07 -0500

    Added menu links and profile extension #68 #49
      - Added menu links in navbar to different pages
      - Extended user profile with default picture
    
    `

--- SA3/qa_web/urls.py
+++ SA3/qa_web/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+
     path(r'login/', views.login, name='login'),
     path(r'logout/', views.logout_view, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
     path('questions/', views.questions, name='questions'),
     path('questions/<int:id_>/', views.answers, name='answers'),
-    url(r'^home/$', views.homepage, name='homepage'),
+    url(r'^$', views.homepage, name='homepage'),
     path('vote/', views.vote, name='vote'),
     path('questions/<int:id_>/edit/', views.edit, name='edit'),
     url(r'QuestionIndex/', views.QuestionDisplayView.as_view(), name='QuestionIndex'),
