b08001ec80d22221d15bd5e088523a639168e102
Alvyn <alvyn279@gmail.com>
2018-03-12 21:58:45 -0400

    Completed forms testing for #82
    - Added some form validation tests for extended coverage

--- SA3/qa_web/tests/test_forms.py
+++ SA3/qa_web/tests/test_forms.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
-from qa_web.forms import UserProfile, QuestionsForm
+from qa_web.models import User
+from qa_web.forms import UserProfile, QuestionsForm, AnswersForm, LoginForm, EditForm
 
+credentials = {'username': 'test', 'password': 'test'}
 
 # Create your tests here.
 class FormTest(TestCase):
+
+    def setUp(self):
+        User.objects.create_user(**credentials)
 
     def test_user_profile(self):
         form_data = {'prename': 'John',
@@ -18,10 +23,41 @@ class FormTest(TestCase):
         form = UserProfile(data=form_data)
         self.assertTrue(form.is_valid())
 
+        invalid_dates= ['1960-08-32', '1960/08/24', '2030-08-32', '1960-24-08']
+
+        for date in invalid_dates:
+            form_data['birthday'] = date
+            form = UserProfile(data=form_data)
+            self.assertJSONEqual(form.errors.as_json(),
+                                 {"birthday": [{"code": "invalid", "message": "Enter a valid date."}]})
+
+
     def test_question(self):
 
         form_data = { 'title': 'How do we query from views content from models in Django?',
                       'content':"When I try to query from `models.py` using filter, I **cannot** access the \
                                 element's attribute. Why is this?"}
-        form =  QuestionsForm(data = form_data)
+        form = QuestionsForm(data = form_data)
         self.assertTrue(form.is_valid())
+
+    def test_answer(self):
+        form_data = { 'content': "Test content for an answer"}
+
+        form = AnswersForm(data= form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_edit(self):
+
+        form_data = { 'content': "Test content for editing a question"}
+        form = EditForm(data= form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_login(self):
+
+        form_data = {'username': None, 'password': None}
+        login = LoginForm(data = form_data)
+        self.assertFalse(login.is_valid())
+        self.assertJSONEqual(login.errors.as_json(), {"password": [{"message": "please input password", "code": "required"}],
+                                                  "__all__": [{"message": "username and password are required", "code": ""}],
+                                                  "username": [{"message": "please input username", "code": "required"}]})
+
