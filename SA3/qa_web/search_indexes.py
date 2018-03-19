419bea59dcb0765c64e0c802980e241fd3ea2f5b
jetcanfly <play_workhard@hotmail.com>
2018-03-19 02:31:59 -0400

    #71. first version for searching title.

--- SA3/qa_web/search_indexes.py
+++ SA3/qa_web/search_indexes.py
@@ -6,12 +6,12 @@ from qa_web.models import Questions
 
 class QuestionsIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    author = indexes.CharField(model_attr='owner')
-    pub_date = indexes.DateTimeField(model_attr='creation_date')
+    owner = indexes.CharField(model_attr='owner')
+    creation_date = indexes.DateTimeField(model_attr='creation_date')
 
     def get_model(self):
         return Questions
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
+        return self.get_model().objects.filter(creation_date__lte=datetime.datetime.now())
